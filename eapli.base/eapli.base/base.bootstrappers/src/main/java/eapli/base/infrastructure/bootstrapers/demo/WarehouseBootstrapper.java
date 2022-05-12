8ecca22c6fe473c3326931a491c0e98ca179bc22
LourencoMelo <1190811@isep.ipp.pt>
2022-05-13 00:51:19 +0100

    <L2S20-8> File Path correcting

--- eapli.base/eapli.base/base.bootstrappers/src/main/java/eapli/base/infrastructure/bootstrapers/demo/WarehouseBootstrapper.java
+++ eapli.base/eapli.base/base.bootstrappers/src/main/java/eapli/base/infrastructure/bootstrapers/demo/WarehouseBootstrapper.java
@@ -11,7 +11,7 @@ public class WarehouseBootstrapper implements Action{
     @Override
     public boolean execute() {
         ImportJSONFile importJSONFile = new ImportJSONFile();
-        File file = new File("eapli.base/Files/warehouse1.json");
+        File file = new File("Files/warehouse1.json");
         WareHousePlant wareHousePlant = importJSONFile.ImportJSONFile(file);
         System.out.println("Imported Default JSON File!");
         return true;
